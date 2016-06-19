680869835513376c4c3145356bc7573b2e9f883a
Lukin Deema <lukin.d8709@gmail.com>
2016-06-19 14:31:51 +0300

    refactor gulp assembly

--- gulpfile.js
+++ gulpfile.js
@@ -3,108 +3,131 @@ var $ = require('gulp-load-plugins')();
 var del = require("del");
 var path = require("path");
 var multipipe = require("multipipe");
+var wiredep = require('wiredep').stream;
+var bowerFiles = require('bower-files')();
 
 var env = process.env.NODE_ENV || 'development';
 var isDev =  env == 'development';
-
+var path = {
+	src: {
+		styles: "frontend/styles/*.sass",
+		assets: "frontend/assets/**",
+	},
+	build:{
+		styles: 'public/assets/css',
+		assets: 'public/assets',
+		bowerFonts: 'public/assets/fonts',
+		bowerJs: 'public/assets/lib',
+	}, 
+	htmlReplace: {
+		styles: [ ],
+		js: [ 'app/angularApp.js', 'app/controllers/appCtrl.js' ]
+	}
+}
 gulp.task("clean", function(){
 
 	return del('public');
 })
 gulp.task('sass', function() {
-  return multipipe(
-  	gulp.src("frontend/styles/*.sass"/*, { since:gulp.lastRun('sass')}*/),
+	return multipipe(
+		gulp.src(path.src.styles),
 		$.if(isDev,$.debug({title:'src'})),
 		$.sourcemaps.init(),
-		//$.remember('sass'),
 		$.sass(),
 		$.concat("all.css"),
 		$.sourcemaps.write(),
-		gulp.dest('public/assets/css')
-	).on("error", $.notify.onError());
+		gulp.dest(path.build.styles)
+		).on("error", $.notify.onError());
 });
 gulp.task('assets', function(){
-	return gulp.src("frontend/assets/**")
-		//.pipe($.cached('assets'))
-		.pipe($.newer('public/assets'))
-		.pipe(gulp.dest('public/assets'));
+	return gulp.src(path.src.assets)
+		.pipe($.newer(path.build.assets))
+		.pipe(gulp.dest(path.build.assets));
 })
 gulp.task('copyMyAngular', function(){
 	return gulp.src("frontend/app/**")
-		.pipe($.newer('public/app'))
-		.pipe(gulp.dest('public/app'));
+	.pipe($.newer('public/app'))
+	.pipe(gulp.dest('public/app'))
 })
-gulp.task("copyLibFonts",function(){
-	return gulp.src("node_modules/bootstrap/fonts/*.*")
-		.pipe($.newer('public'))
-		.pipe(gulp.dest('public/assets/fonts'));	
+gulp.task("copyBowerFonts",function(){
+	return gulp.src(bowerFiles.ext(['eot', 'woff', 'woff2', 'ttf', 'svg']).files)
+    .pipe(gulp.dest(path.build.bowerFonts));
 })
-gulp.task('copyLibJs', function(){
-	return gulp.src(["node_modules/bootstrap/dist/js/bootstrap.js",
-		"node_modules/angular-ui-router/release/angular-ui-router.js",
-		"node_modules/angular/angular.js"])
-		.pipe($.newer('public/assets/lib'))
-		.pipe(gulp.dest('public/assets/lib'))///
+gulp.task('copyBowerJs', function(){
+	return gulp.src(bowerFiles.ext('js').files)
+    .pipe(gulp.dest(path.build.bowerJs));
 })
-gulp.task('copyLibCss', function(){
-	return gulp.src(["node_modules/bootstrap/dist/css/bootstrap.css"])
-		.pipe($.newer('public/assets/css'))
-		.pipe(gulp.dest('public/assets/css'))///
+gulp.task('copyBowerCss', function(){
+	return gulp.src(bowerFiles.ext('css').files)
+    .pipe(gulp.dest(path.build.styles));
 })
-gulp.task('copyIndexHtml', function(){
-	return gulp.src(["frontend/index.html"])
-		.pipe($.newer('public/assets/css'))
-		.pipe(gulp.dest('public'))///
-})
+
+gulp.task('processHtml', function () {
+	return gulp.src('frontend/index.html')
+	.pipe(wiredep({src: "public/assets/",
+		ignorePath: /[a-zA-Z_\-\.]+\//g,
+		fileTypes: {
+			html: {
+				replace: {
+					js: '<script src="assets/lib/{{filePath}}"></script>',
+					css: '<link rel="stylesheet" href="assets/css/{{filePath}}" />',
+				}
+			}
+		}
+	}))
+	.pipe($.htmlReplace({
+		'css':{
+			src: path.htmlReplace.styles,
+			tpl:'<link rel="stylesheet" href="%s" />'
+		},
+		'js':{
+			src: path.htmlReplace.js,
+			tpl:'<script type="text/javascript" src="%s" charset="UTF-8"></script>'
+		}
+	}, { keepBlockTags: true }))
+	.pipe(gulp.dest('public'));
+});
+
 gulp.task("watch",function(){
 	gulp.watch("frontend/styles/*.sass", gulp.series("sass"))
-		.on("unlink",function(filename){
-			$.remember.forget('sass', path.resolve(filename));
-			//delete $.cached.caches.sass[path.resolve(filename)];
-	})
 	gulp.watch("frontend/assets/**", gulp.series("assets"))
-		.on("unlink",function(filename){
-			$.remember.forget('assets', path.resolve(filename));
-			//delete $.cached.caches.assets[path.resolve(filename)];
-	})
 });
 
 gulp.task('startServer', function() {
 	$.nodemon({
-    script: 'app.js',
-    ext: 'html js',
-    env: {
-      'NODE_ENV': 'development'
-    },
-    ignore: [
-      'node_modules/',
-      'bower_components/',
-      'logs/',
-      'packages/*/*/public/assets/lib/',
-      'packages/*/*/node_modules/',
-      '.DS_Store', '**/.DS_Store',
-      '.bower-*',
-      '**/.bower-*',
-      '**/tests'
-    ],
-    nodeArgs: ['--debug'],
-    stdout: false
-  }).on('readable', function() {
-    this.stdout.on('data', function(chunk) {
-      if (/Mean app started/.test(chunk)) {
-        setTimeout(function() {
-          plugins.livereload.reload();
-        }, 500);
-      }
-      process.stdout.write(chunk);
-    });
-    this.stderr.pipe(process.stderr);
-  });
+		script: 'app.js',
+		ext: 'html js',
+		env: {
+			'NODE_ENV': 'development'
+		},
+		ignore: [
+		'node_modules/',
+		'bower_components/',
+		'logs/',
+		'packages/*/*/public/assets/lib/',
+		'packages/*/*/node_modules/',
+		'.DS_Store', '**/.DS_Store',
+		'.bower-*',
+		'**/.bower-*',
+		'**/tests'
+		],
+		nodeArgs: ['--debug'],
+		stdout: false
+	}).on('readable', function() {
+		this.stdout.on('data', function(chunk) {
+			if (/Mean app started/.test(chunk)) {
+				setTimeout(function() {
+					plugins.livereload.reload();
+				}, 500);
+			}
+			process.stdout.write(chunk);
+		});
+		this.stderr.pipe(process.stderr);
+	});
 });
 
 gulp.task('default', gulp.series(
-	"clean", 
-	gulp.parallel("sass","assets","copyLibJs", "copyIndexHtml",
-		"copyLibCss","copyMyAngular","copyLibFonts"),
+	"clean",
+	gulp.parallel("sass", "assets", "copyBowerJs", "copyBowerCss", "copyMyAngular", "copyBowerFonts","processHtml"),
 	gulp.parallel("startServer", "watch")
 ));
