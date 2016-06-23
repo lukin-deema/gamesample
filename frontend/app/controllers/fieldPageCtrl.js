ad831c2117d7c84d983925cc042c93470d96fcdf
Lukin Deema <lukin.d8709@gmail.com>
2016-06-23 21:14:07 +0300

    multiplayer beta

--- frontend/app/controllers/fieldPageCtrl.js
+++ frontend/app/controllers/fieldPageCtrl.js
@@ -15,41 +15,58 @@ angular.module('myApp.controllers').controller('fieldPageCtrl',
 			W : function(name, code) { $scope.move(0, -10); },
 			D : function(name, code) { $scope.move(+10, 0); }
 		};
-		socket.emit("init", $scope.userCircle[0], function(users){
-			$scope.renderUsers(user);
-		});
 		$scope.move = function(dx, dy){
 			$scope.userCircle[0].x = $scope.userCircle[0].x + dx;
 			$scope.userCircle[0].y = $scope.userCircle[0].y + dy;
-			socket.emit('move', $scope.userCircle[0], 
+			socket.emit('user:move', $scope.userCircle[0], 
 				function (user) {
 					console.log("emit move: " + JSON.stringify(user));
 			});
 		}
-		socket.on("move", function (user) {
+		socket.on("user:move", function (user) {
 			console.log("on move: "+ JSON.stringify(user));
-			$scope.renderUsers(user);
-		})
-		socket.on("init", function(users){
-			for (var i = 0; i < users.length; i++) {
-				$scope.renderUsers(users[i]);
+			//$scope.renderUser(user);
+			for (var i = 0; i < $scope.userCircle.length; i++) {
+				if ($scope.userCircle[i].name == user.user.name){
+					break
+				}
 			}
+			$scope.userCircle[i].x = user.user.x;
+			$scope.userCircle[i].y = user.user.y;
 		})
-		$scope.renderUsers = function (user){
+		socket.on("me:connect", function(users){
+			users.forEach(function(user){
+				$scope.renderUser(user);
+			})
+			socket.emit("me:rename", $scope.userCircle[0])
+		})
+		socket.on("user:connect", function(user){
+			$scope.renderUser(user);
+		})
+		socket.on("user:left",function(user){
+			console.log("user:left " + JSON.stringify(user));
+			var i = $scope.indexOf(user.user.name);
+			$scope.userCircle.splice(i, 1);
+		})
+		$scope.indexOf = function(usrName){
+			for (var i = 0; i < $scope.userCircle.length; i++) {
+				if ($scope.userCircle[i].name == usrName) {
+					break;
+				}
+			}
+			return i;
+		}
+		$scope.renderUser = function (user){
 			if (!$scope.userCircle.some(function(el){ return el.name == user.name; })){
-				$scope.userCircle = $scope.userCircle.concat(user);
+				$scope.userCircle = $scope.userCircle.concat(user.user);
 			}
 			var usr = $scope.userCircle.filter(function(el){ 
 				return el.name == user.name;
 			})
 			if (usr.length == 1) {
-				for (var i = 0; i < $scope.userCircle.length; i++) {
-					if ($scope.userCircle[i].name == usr[0].name) {
-						break;
-					}
-				}
-				$scope.userCircle[i].x = user.x;
-				$scope.userCircle[i].y = user.y;
+				var i = $scope.indexOf(usr[0].name);
+				$scope.userCircle[i].x = user.user.x;
+				$scope.userCircle[i].y = user.user.y;
 			}
 		}
 	}
--- socket/index.js
+++ socket/index.js
@@ -1,31 +1,38 @@
 module.exports = function(server){
 	var io = require('socket.io').listen(server);
-	var users = [];
+	var users = []; //{id: socketId, user: user without isMe prop}
 	io.set("origins", "localhost:*");
 	io.sockets.on("connection",function(socket){
-	  console.log('connection');
-	  socket.on("move",function(user,cb){
-	  	console.log("income user" + JSON.stringify(user));
-	  	console.log("all users" + JSON.stringify(users));
-	    var findUser = users.find(function(el){ 
-	    	return el.name == user.name;
-	    });
-	    findUser.x = user.x;
-	  	findUser.y = user.y;
-	  	console.log("found user" + JSON.stringify(findUser));
-	  	socket.broadcast.emit("move", findUser);
-	  	cb("back from server " + JSON.stringify(findUser));
-	  })
-	  socket.on("init", function(user){
-	  	user.isMe = undefined;
-	  	console.log("init " + JSON.stringify(user));
-	  	socket.emit("init", users);
-	  	users.push(user);
-	  })
-	  socket.on('disconnect', function(){
-	  	console.log(JSON.stringify(arguments));
-	    console.log('disconnected');
-	  });
+		console.log('connection: ' + socket.id);
+
+		io.sockets.sockets[socket.id].emit("me:connect", users);
+
+		socket.on("me:rename", function(user){
+			user.isMe = undefined;
+			console.log("init " + JSON.stringify(user));
+			users.push({id: socket.id, user: user});
+			socket.broadcast.emit("user:connect", {id: socket.id, user: user});
+		})
+		socket.on("user:move",function(user,cb){
+			var findUser = users.find(function(el){ 
+				return el.user.name == user.name;
+			});
+			findUser.user.x = user.x;
+			findUser.user.y = user.y;
+			console.log("income user" + JSON.stringify(user)+ ",\tall users" + JSON.stringify(users) + "\tfound user" + JSON.stringify(findUser));
+			socket.broadcast.emit("user:move", findUser);
+		})
+		socket.on("disconnect", function(){
+			var u = users.filter(function(el){
+				return el.id == socket.id;
+			})[0];
+			users.splice(users.indexOf(u), 1);
+			console.log('disconnected: '+ JSON.stringify(u) + ",\tall users" + JSON.stringify(users));
+			socket.broadcast.emit('user:left', u);
+		});
+		socket.on("error",function(){
+			debugger;
+		})
 	})
 
 	return io;
